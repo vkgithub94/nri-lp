20e90b1662e9a6541395e93bae51932002108068
Vikash <vikashp@orowealth.com>
2019-12-25 15:35:11 +0530

    Updates

---
+++ precache-manifest.8aee4e0cf2e5ee3c58e908d980a77f23.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94c91c5906101ce942f149f374e64df4",
+    "url": "/nri-lp/index.html"
+  },
+  {
+    "revision": "24765b170862100b9390",
+    "url": "/nri-lp/static/css/2.ac443e36.chunk.css"
+  },
+  {
+    "revision": "9f17a13ebbfb8aa26aa3",
+    "url": "/nri-lp/static/css/main.ac0fae8d.chunk.css"
+  },
+  {
+    "revision": "24765b170862100b9390",
+    "url": "/nri-lp/static/js/2.fc9edf02.chunk.js"
+  },
+  {
+    "revision": "9f17a13ebbfb8aa26aa3",
+    "url": "/nri-lp/static/js/main.6ff93dc6.chunk.js"
+  },
+  {
+    "revision": "e21cd1fcac769241d894",
+    "url": "/nri-lp/static/js/runtime-main.b178f78e.js"
+  },
+  {
+    "revision": "9cf91a7190526613540e4a85289bdcca",
+    "url": "/nri-lp/static/media/chat-bg.9cf91a71.svg"
+  },
+  {
+    "revision": "b160b4229ccfb22403ab73a2e3e48a62",
+    "url": "/nri-lp/static/media/chat.b160b422.png"
+  },
+  {
+    "revision": "e94bf0d742a647bc79dafffe579b5b1a",
+    "url": "/nri-lp/static/media/graph-bg.e94bf0d7.svg"
+  },
+  {
+    "revision": "d7adb1f73dfb5f7d13eeef91a5ff3e83",
+    "url": "/nri-lp/static/media/ic-alert.d7adb1f7.svg"
+  },
+  {
+    "revision": "f008fae49dedcc01af32043855e1aa25",
+    "url": "/nri-lp/static/media/img-guy.f008fae4.svg"
+  },
+  {
+    "revision": "cf3be770149d298cccd7cf9f4f380e1d",
+    "url": "/nri-lp/static/media/li1.cf3be770.svg"
+  },
+  {
+    "revision": "4359a100a90079ab4059c7dcaa7a9569",
+    "url": "/nri-lp/static/media/li2.4359a100.svg"
+  },
+  {
+    "revision": "df26498a0a0c7a03985482c0e2883e36",
+    "url": "/nri-lp/static/media/li3.df26498a.svg"
+  },
+  {
+    "revision": "2a5321ad387719003da511d47306deb2",
+    "url": "/nri-lp/static/media/map.2a5321ad.svg"
+  },
+  {
+    "revision": "02d69a4230254e207a82cbcd94ca4536",
+    "url": "/nri-lp/static/media/pttrn.02d69a42.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/nri-lp/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/nri-lp/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/nri-lp/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/nri-lp/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "86643d4ad1b3eb529069b3bdbdd77587",
+    "url": "/nri-lp/static/media/testimonial-bg-lines.86643d4a.svg"
+  },
+  {
+    "revision": "628e506a750809adba17fb276163ef22",
+    "url": "/nri-lp/static/media/triangles.628e506a.svg"
+  },
+  {
+    "revision": "35bd32e7c26810c03f9b5adab09ae49c",
+    "url": "/nri-lp/static/media/“.35bd32e7.svg"
+  }
+]);
